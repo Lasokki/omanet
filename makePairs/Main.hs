4ae03f6ac854d7ba90fdae9d88fedddca20264af
Tuukka Wahtera <tuukka.wahtera@futurice.com>
2017-06-17 19:58:39 +0300

    Begin making standalone executable to make pairings

--- makePairs/Main.hs
+++ makePairs/Main.hs
@@ -1,7 +1,25 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import Prelude     (IO)
-import Data.Text.IO
+import Import hiding (on, (==.))
+import Application (db)
+
+import Database.Esqueleto
+
+testPair :: Guest -> Host -> Bool
+testPair g h = not (not (hostAllowSmoking h) && (guestSmokes g))
+
+getEntityUserName :: Entity User -> Text
+getEntityUserName (Entity _ u) = userName u
 
 main :: IO ()
-main = putStrLn "hello"
+main = do
+    hosts <- db $ select $ from $ \(user `InnerJoin` host) ->
+        do on $ user ^. UserId ==. host ^. HostUserId
+           return  (user, host)
+    
+    let names = map (getEntityUserName . fst) hosts
+    mapM_ putStrLn names
+    
